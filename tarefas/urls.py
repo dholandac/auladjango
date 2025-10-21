bb673d7c7c8cda2d8fd44a9ee6cd14a27aa79258
dholandac <daniel.holanda.campos@gmail.com>
2025-10-21 16:55:36 -0300

    remoto

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('adicionar/', views.add, name='adicionar')
+    path('adicionar/', views.add, name='adicionar'),
+    path('remover/', views.remove, name='remover'),
+    path('editar/', views.edit, name='editar'),
+    path('buscar/', views.search, name='buscar')
 ]
