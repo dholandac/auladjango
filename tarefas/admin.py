61203af2b23bdd23b4030f2020c856c38f79bcba
dholandac <daniel.holanda.campos@gmail.com>
2025-10-22 17:18:20 -0300

    integrando models

--- tarefas/admin.py
+++ tarefas/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Tarefa
 
-# Register your models here.
+class TarefaAdmin(admin.ModelAdmin):
+    list_display = ('titulo','descricao','criada_em','concluida')
+admin.site.register(Tarefa, TarefaAdmin)
