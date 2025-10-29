5e6e70af674a2de5a84ef43c885426cffbbc47a6
dholandac <daniel.holanda.campos@gmail.com>
2025-10-29 15:03:54 -0300

    adicionando perfis

--- tarefas/admin.py
+++ tarefas/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-from .models import Tarefa
+from .models import Tarefa, Etiqueta, Perfil
 
 class TarefaAdmin(admin.ModelAdmin):
     list_display = ('titulo','descricao','criada_em','concluida')
+
 admin.site.register(Tarefa, TarefaAdmin)
+
+class EtiquetaAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'cor']
+
+admin.site.register(Etiqueta, EtiquetaAdmin)
+
+class PerfilAdmin (admin.ModelAdmin):
+    list_display = ['bio', 'tema']
+
+admin.site.register(Perfil, PerfilAdmin)
--- tarefas/models.py
+++ tarefas/models.py
@@ -1,12 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Etiqueta (models.Model):
+    nome = models.CharField(unique=True)
+    cor = models.CharField(default="#000000")
+
+    def str(self):
+        return self.nome
+
 class Tarefa(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField(blank=True)
     concluida = models.BooleanField(default=False)
     criada_em = models.DateTimeField(auto_now_add=True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    etiquetas = models.ManyToManyField(Etiqueta, blank=True)
 
     def __str__(self):
         return self.titulo
+    
+class Perfil (models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    tema = models. BooleanField(default=False)
+
+    def str(self):
+        return f"Perfil de {self.usuario.username}"
